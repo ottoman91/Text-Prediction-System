3ce223ca416388cbb81188d6fd83f7e2cd94dfb8
Usman Khaliq <usman.khaliq91@gmail.com>
2016-04-24 23:57:53 +0000

    first script

---
+++ getdata.R
@@ -0,0 +1,47 @@
+#load specific libraries
+suppressPackageStartupMessages(library(tm))
+suppressPackageStartupMessages(library(NLP))
+
+#unzip file 
+dataUrl <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+download.file(dataUrl, destfile = "Coursera-SwiftKey.zip", method = "wget")
+unzip("Coursera-SwiftKey.zip",exdir="./") 
+#set working directory
+setwd("./final") 
+setwd("./en_US/")
+#create connections for all files
+samplingFile <- function(filename, prob) {
+  incon <- file(paste("en_US.", filename, ".txt",sep=""),"r")
+  file <- readLines(incon)
+  # sampling by rbinom()
+  set.seed(123)
+  sample_file <- file[rbinom(n = length(file), size = 1, prob = prob) == 1]
+  close(incon)
+  
+  # Write out the sample file to the local file to save it
+  outCon <- file(paste("sample_", filename, ".txt",sep=""), "w")
+  writeLines(sample_file, con = outCon)
+  close(outCon)
+} 
+
+#create the sample files for all three files
+samplingFile("blogs", 0.6) 
+samplingFile("news",0.6) 
+samplingFile("twitter",0.6)  
+
+#create the R Data frames for each line
+twitter_file <- file("sample_twitter.txt", "r") 
+twitterData <- readLines(twitter_file) 
+close(twitter_file) 
+save(twitterData, file = "twitterData.RData") 
+
+news_file <- file("sample_news.txt", "r")
+newsData <- readLines(news_file)
+close(news_file)
+save(newsData, file = "newsData.RData") 
+
+blogs_file <- file("sample_blogs.txt", "r")
+blogsData <- readLines(blogs_file)
+close(blogs_file)
+save(blogsData, file="blogsData.RData")
+
